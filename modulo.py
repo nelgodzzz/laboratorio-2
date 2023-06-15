5e5148c0b182a10c3cc629d5f59a7ac52ff9629d
nelgodzzz <128824494+nelgodzzz@users.noreply.github.com>
2023-06-15 08:47:10 -0600

    operacion de suma

--- modulo.py
+++ modulo.py
@@ -1,1 +1,10 @@
-import math
+
+
+# suma.py
+
+a = float(input("Ingresa el primer número: "))
+b = float(input("Ingresa el segundo número: "))
+
+resultado = a + b
+
+print(f"El resultado de la suma es: {resultado}")
