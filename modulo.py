800492b4ae5dabf22eb1d31e119a87a8b616de9c
nelgodzzz <128824494+nelgodzzz@users.noreply.github.com>
2023-06-15 09:11:33 -0600

    resta

--- modulo.py
+++ modulo.py
@@ -8,3 +8,12 @@ b = float(input("Ingresa el segundo número: "))
 resultado = a + b
 
 print(f"El resultado de la suma es: {resultado}")
+
+# resta.py
+
+a = float(input("Ingresa el primer número: "))
+b = float(input("Ingresa el segundo número: "))
+
+resultado = a - b
+
+print(f"El resultado de la resta es: {resultado}")
